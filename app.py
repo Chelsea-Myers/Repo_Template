c03e6031fedf045ce0d971aeecf7d5cc26ebd611
ChelseaMyers <chelsea.e.myers@gmail.com>
2025-04-18 13:41:21 -0400

    Add matplotlib and visualization

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('ds_salaries.csv')
 
-print(df.head())
+df['company_size'].value_counts().plot(kind='bar')
+
+plt.tight_layout()
+plt.savefig('/mnt/c/Users/chelseam/OneDrive - Full Sail University/Desktop/Data Visualization and Modeling/Repo_Template/company_size.png')  # Save to a Windows-accessible location
+plt.close()
